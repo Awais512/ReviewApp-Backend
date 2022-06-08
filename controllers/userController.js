1fca54e76d6a1c39cadfdc314538737cd928393d
awais512 <raza.awais25@gmail.com>
2022-06-08 13:55:12 +0500

    Preventing Duplicate User

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,10 @@ import User from '../models/userModel.js';
 //Register User
 const register = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
+  const existUser = await User.findOne({ email });
+  if (existUser) {
+    return res.status(400).json({ error: 'Email is already exist' });
+  }
   const user = await User.create({ name, email, password });
   res.status(201).json(user);
 });
