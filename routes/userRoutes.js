8c1c4f9daf0c6fe3446ed76974f37ac584c6bbfa
awais512 <raza.awais25@gmail.com>
2022-06-24 01:23:30 -0700

    Auth Middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const {
   register,
   login,
@@ -7,35 +7,48 @@ const {
   forgotPassword,
   sendResetPasswordTokenStatus,
   resetPassword,
-} = require('../controllers/userController');
-const { isVlidPsswordResetToken } = require('../middlewares/userMiddleware');
+} = require("../controllers/userController");
+const { isAuth } = require("../middlewares/auth");
+const { isVlidPsswordResetToken } = require("../middlewares/userMiddleware");
 const {
   userValidtor,
   validatePassword,
   validate,
   signInValidator,
-} = require('../middlewares/validator');
+} = require("../middlewares/validator");
 
 const router = express.Router();
 
-router.post('/register', userValidtor, validate, register);
-router.post('/login', signInValidator, validate, login);
-router.post('/verify', verifyEmail);
-router.post('/resend-email-verification-token', resendEmailVerificationToken);
-router.post('/forgot-password', forgotPassword);
+router.post("/register", userValidtor, validate, register);
+router.post("/login", signInValidator, validate, login);
+router.post("/verify", verifyEmail);
+router.post("/resend-email-verification-token", resendEmailVerificationToken);
+router.post("/forgot-password", forgotPassword);
 router.post(
-  '/verify-pass-reset-token',
+  "/verify-pass-reset-token",
   isVlidPsswordResetToken,
   (req, res, next) => {
     res.json({ valid: true });
   }
 );
 router.post(
-  '/reset-password',
+  "/reset-password",
   validatePassword,
   validate,
   isVlidPsswordResetToken,
   resetPassword
 );
+
+router.get("/is-auth", isAuth, (req, res) => {
+  const { user } = req;
+  res.json({
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isVerified: user.isVerified,
+    },
+  });
+});
 
 module.exports = router;
