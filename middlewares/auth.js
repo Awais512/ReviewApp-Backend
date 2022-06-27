1479c0ab8666a59687af8391dee31de076d3f56f
awais512 <raza.awais25@gmail.com>
2022-06-27 02:09:50 -0700

    use admin middleware in routes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,6 +5,7 @@ const User = require("../models/userModel");
 exports.isAuth = async (req, res, next) => {
   const token = req.headers?.authorization;
 
+  if (!token) return sendError(res, "Invalid token!");
   const jwtToken = token.split("Bearer ")[1];
 
   if (!jwtToken) return sendError(res, "Invalid token!");
@@ -12,9 +13,10 @@ exports.isAuth = async (req, res, next) => {
   const { userId } = decode;
 
   const user = await User.findById(userId);
-  if (!user) return sendError(res, "Invalid token user not found!", 404);
+  if (!user) return sendError(res, "unauthorized access!");
 
   req.user = user;
+
   next();
 };
 
--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -9,11 +9,14 @@ const {
 } = require("../controllers/actorController");
 const { uploadImage } = require("../middlewares/multer");
 const { actorInfoValidator, validate } = require("../middlewares/validator");
+const { isAuth, isAdmin } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post(
   "/",
+  isAuth,
+  isAdmin,
   uploadImage.single("avatar"),
   actorInfoValidator,
   validate,
@@ -22,13 +25,15 @@ router.post(
 
 router.post(
   "/:actorId",
+  isAuth,
+  isAdmin,
   uploadImage.single("avatar"),
   actorInfoValidator,
   validate,
   updateActor
 );
 
-router.delete("/:actorId", deleteActor);
+router.delete("/:actorId", isAuth, isAdmin, deleteActor);
 router.get("/search", searchActor);
 router.get("/", getLatestActors);
 router.get("/:id", getSingleActor);
