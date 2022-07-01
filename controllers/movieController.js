aae2309ffbcfeb47cf26f4470bc0ae68bfc12a3c
awais512 <raza.awais25@gmail.com>
2022-07-01 13:12:01 +0500

    Upload Trailer

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,8 +1,18 @@
 const asyncHandler = require("express-async-handler");
 const Movie = require("../models/movieModel");
+const { sendError } = require("../utils/helper");
+const cloudinary = require("../cloud");
 
 const uploadTrailer = asyncHandler(async (req, res) => {
-  res.send("Send");
+  const { file } = req;
+  if (!file) return sendError(res, "Video file is missing");
+  const { secure_url: url, public_id } = await cloudinary.uploader.upload(
+    file.path,
+    {
+      resource_type: "video",
+    }
+  );
+  res.status(201).json({ url, public_id });
 });
 
 module.exports = { uploadTrailer };
